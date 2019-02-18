b6ae1d1adb6c8afc84c42f109b5456c559b79a41
kawaguchisouya <kawakawa.410487@gmail.com>
2019-02-18 17:45:37 +0900

    [add] serializable

--- app/src/main/java/karatakushi/io/lfs_/Item.java
+++ app/src/main/java/karatakushi/io/lfs_/Item.java
@@ -1,4 +1,18 @@
 package karatakushi.io.lfs_;
 
-public class Item {
+import java.io.Serializable;
+
+public class Item implements Serializable {
+
+    String title;
+    String content;
+    int likeCount;
+    boolean isLiked;
+
+    Item(String title, String content, int likeCount) {
+        this.title = title;
+        this.content = content;
+        this.likeCount = likeCount;
+        this.isLiked = false;
+    }
 }
--- app/src/main/java/karatakushi/io/lfs_/MainActivity.java
+++ app/src/main/java/karatakushi/io/lfs_/MainActivity.java
@@ -1,7 +1,11 @@
 package karatakushi.io.lfs_;
 
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public List<ClipData.Item> getSampleData() {
+        List<Item> items = new ArrayList<>();
+
+        items.add(new Item("タイトル1", "内容1", 1));
+        items.add(new Item("タイトル2", "内容2", 2));
+        items.add(new Item("タイトル3", "内容3", 3));
+        items.add(new Item("タイトル4", "内容4", 4));
+        items.add(new Item("タイトル5", "内容5", 5));
+        items.add(new Item("タイトル6", "内容6", 6));
+        items.add(new Item("タイトル7", "内容7", 7));
+        items.add(new Item("タイトル8", "内容8", 8));
+        items.add(new Item("タイトル9", "内容9", 9));
+
+        return items;
     }
 }
